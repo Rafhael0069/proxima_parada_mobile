277e075a171fafa8e75dcce4a310bac182f94b51
RafhaelGaspar <102383572+Rafhael0069@users.noreply.github.com>
2024-09-03 09:36:10 -0300

    :recycle: refactor: Reconfigurando o projeto para verção mais recente do flutter e atualizando as bibliotecas

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -13,14 +13,17 @@ class ProfilePage extends StatelessWidget {
   ProfilePage({super.key, required this.userId});
 
   final _phoneMask = MaskTextInputFormatter(
-      mask: '(##) # ####-####', filter: {"#": RegExp(r'[0-9]')}, type: MaskAutoCompletionType.lazy);
+      mask: '(##) # ####-####',
+      filter: {"#": RegExp(r'[0-9]')},
+      type: MaskAutoCompletionType.lazy);
 
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
     return FutureBuilder(
       future: FirebaseService().getUserData(userId, context),
-      builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot?> snapshot) {
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot?> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(
             child: CircularProgressIndicator(),
@@ -35,149 +38,164 @@ class ProfilePage extends StatelessWidget {
           );
         } else {
           // Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
-          LocalUser userData = LocalUser.fromMap(snapshot.data!.data() as Map<String, dynamic>);
-          return Container(
-            height: mediaQuery.size.height,
-            padding: const EdgeInsets.all(16),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 50),
-                        child: userData.locationImage != null
-                            ? CircleAvatar(
-                                radius: 100,
-                                backgroundImage: NetworkImage(userData.locationImage!),
-                                backgroundColor: Colors.white,
-                              )
-                            : const CircleAvatar(
-                                radius: 100,
-                                backgroundImage: AssetImage('assets/images/user_avatar.png'),
-                                backgroundColor: Colors.white,
-                              ),
-                      ),
-                      Row(
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 12),
-                            child: Icon(
-                              Icons.perm_identity_outlined,
-                              size: 50,
-                            ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text("Nome",
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  )),
-                              Text(
-                                userData.name!,
-                                style: const TextStyle(
-                                  fontSize: 18,
+          LocalUser userData =
+              LocalUser.fromMap(snapshot.data!.data() as Map<String, dynamic>);
+          return SingleChildScrollView(
+            child: Container(
+              height: mediaQuery.size.height,
+              padding: const EdgeInsets.all(16),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 50),
+                          child: userData.locationImage != null
+                              ? CircleAvatar(
+                                  radius: 100,
+                                  backgroundImage:
+                                      NetworkImage(userData.locationImage!),
+                                  backgroundColor: Colors.white,
+                                )
+                              : const CircleAvatar(
+                                  radius: 100,
+                                  backgroundImage: AssetImage(
+                                      'assets/images/user_avatar.png'),
+                                  backgroundColor: Colors.white,
                                 ),
+                        ),
+                        Row(
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 12),
+                              child: Icon(
+                                Icons.perm_identity_outlined,
+                                size: 50,
                               ),
-                            ],
-                          )
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 12),
-                            child: Icon(
-                              Icons.phone_outlined,
-                              size: 50,
                             ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text("Telefone",
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  )),
-                              Text(
-                                _phoneMask.maskText(userData.phoneNumber!),
-                                style: const TextStyle(
-                                  fontSize: 18,
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text("Nome",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                                Text(
+                                  userData.name!,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                  ),
                                 ),
+                              ],
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 12),
+                              child: Icon(
+                                Icons.phone_outlined,
+                                size: 50,
                               ),
-                            ],
-                          )
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 12),
-                            child: Icon(
-                              Icons.email_outlined,
-                              size: 50,
                             ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text("Email",
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  )),
-                              Text(
-                                userData.email!, // Email do usuário
-                                style: const TextStyle(
-                                  fontSize: 18,
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text("Telefone",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                                Text(
+                                  _phoneMask.maskText(userData.phoneNumber!),
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                  ),
                                 ),
+                              ],
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 12),
+                              child: Icon(
+                                Icons.email_outlined,
+                                size: 50,
                               ),
-                            ],
-                          )
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                    ],
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(45)),
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => EditProfilePage(userId: userId)));
-                    },
-                    child: const Text('Editar Perfil',
-                        style: TextStyle(
-                          fontSize: 20,
-                        )),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(45)),
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => CreateAndEditVehicle(userId: userId)));
-                      // ShowAlertDialog.showAlertDialog(context, "Desculpe, ainda não implementado :(");
-                    },
-                    child: const Text('Adicionar veiculo',
-                        style: TextStyle(
-                          fontSize: 20,
-                        )),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(45)),
-                    onPressed: () {
-                      ShowAlertDialog.showAlertDialog(context, "Desculpe, ainda não implementado :(");
-                    },
-                    child: const Text('Quero oferecer caronas',
-                        style: TextStyle(
-                          fontSize: 20,
-                        )),
-                  ),
-                ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text("Email",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                                Text(
+                                  userData.email!, // Email do usuário
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                      ],
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(45)),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    EditProfilePage(userId: userId)));
+                      },
+                      child: const Text('Editar Perfil',
+                          style: TextStyle(
+                            fontSize: 20,
+                          )),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(45)),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    CreateAndEditVehicle(userId: userId)));
+                        // ShowAlertDialog.showAlertDialog(context, "Desculpe, ainda não implementado :(");
+                      },
+                      child: const Text('Adicionar veiculo',
+                          style: TextStyle(
+                            fontSize: 20,
+                          )),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(45)),
+                      onPressed: () {
+                        ShowAlertDialog.showAlertDialog(
+                            context, "Desculpe, ainda não implementado :(");
+                      },
+                      child: const Text('Quero oferecer caronas',
+                          style: TextStyle(
+                            fontSize: 20,
+                          )),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
